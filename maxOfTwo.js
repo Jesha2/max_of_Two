a2d5475367ceef8ab20fb02d4949cab5798d98d3
Fajesha Nasim <113140942+Jesha2@users.noreply.github.com>
2023-06-21 15:57:01 -0500

    Added files via upload

---
+++ maxOfTwo.js
@@ -0,0 +1,15 @@
+let x = 11
+let y = 11
+
+// Write your code below this line.
+console.log("To find max of 2 numbers");
+if (x > y) {
+  console.log(x);
+}
+else if (y > x) {
+  console.log(y);
+}
+else {
+  console.log(`Both ${x} and ${y} are the same`);
+}
+
